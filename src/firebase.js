ba3d9fa38e6c5d4268f2c9113fb050804487b0ff
rajarahul12 <maddula.rajarahul@gmail.com>
2020-10-22 01:58:05 +0530

    Adding firebase and cleanup

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { fireEvent } from "@testing-library/react";
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmWmFvBFspReatR9HNQSRsco9tSWcQvvc",
+  authDomain: "react-clone-discord.firebaseapp.com",
+  databaseURL: "https://react-clone-discord.firebaseio.com",
+  projectId: "react-clone-discord",
+  storageBucket: "react-clone-discord.appspot.com",
+  messagingSenderId: "809630687948",
+  appId: "1:809630687948:web:0dcd23ee8ef947b4a35869",
+  measurementId: "G-5QRZ77TJSS"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
